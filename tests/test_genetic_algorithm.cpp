05547b9c018e7a2f504be20dd04a76e355bb0104
esemsc-yj1024 <yilin.jing24@imperial.ac.uk>
2025-05-20 16:32:46 +0100

    docs: Add TODO for accuracy problem in genetic algorithm test

--- tests/test_genetic_algorithm.cpp
+++ tests/test_genetic_algorithm.cpp
@@ -416,6 +416,8 @@ int main() {
   }
   std::cout << "Original Discrete GA test PASSED\n\n";
 
+
+  // TODO: deal with the accuracy problem
   // // --- Continuous test ---
   // std::vector<double> vector2 = {0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6,
   //                                0.7, 0.8, 0.9, 0.2, 0.3, 0.4, 0.5,
